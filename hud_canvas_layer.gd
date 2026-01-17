c9c53f485cfcb400bc88438e011d064487b1b9e1
hkeane-laptop <henry@keane-house.com>
2026-01-16 20:09:23 -0800

    Progress Bar Visuals

--- hud_canvas_layer.gd
+++ hud_canvas_layer.gd
@@ -1,11 +1,22 @@
 extends CanvasLayer
 @onready var progress_bar: ProgressBar = %ProgressBar
+@onready var radial_progress_bar: TextureProgressBar = %RadialProgressBar
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	EventBus.sprint_changed.connect(sprint_changed)
-	pass # Replace with function body.
+	EventBus.interaction_progress.connect(interaction_progress_changed)
+	EventBus.interaction_complete.connect(interaction_complete)
+	
 
-func sprint_changed(sprint_meter):
+func sprint_changed(sprint_meter) -> void:
 	progress_bar.value = sprint_meter
+
+func interaction_progress_changed(progress) -> void:
+	radial_progress_bar.visible = true
+	radial_progress_bar.value = progress
+
+func interaction_complete() -> void:
+	print("done!")
+	radial_progress_bar.visible = false
--- player_character.gd
+++ player_character.gd
@@ -14,9 +14,36 @@ var sprinting = false
 const SPEED = 2.0
 const SPRINT_SPEED = 5.0
 
+@export var interaction_hold_time: float = 2.0  # Time in seconds to hold for interaction
+var interaction_hold_timer: float = 0.0
+var is_interacting: bool = false
+
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
+func _process(delta: float) -> void:
+	# Handle interaction hold mechanic
+	if Input.is_action_just_pressed("interact") and not is_interacting:
+		interaction_hold_timer = 0.0
+		is_interacting = true
+	
+	if Input.is_action_pressed("interact") and is_interacting:
+		interaction_hold_timer += delta
+		var progress = interaction_hold_timer / interaction_hold_time * 100
+		EventBus.interaction_progress.emit(progress)
+		
+		if interaction_hold_timer >= interaction_hold_time:
+			_stop_interacting()
+			_complete_interaction()
+			
+	if Input.is_action_just_released("interact"):
+		_stop_interacting()
+
+func _stop_interacting() -> void:
+	is_interacting = false
+	EventBus.interaction_progress.emit(0.0)
+	interaction_hold_timer = 0.0
+	
 func _input(event: InputEvent):
 	# Handle mouse movement for looking around
 	if event is InputEventMouseMotion:
@@ -76,3 +103,9 @@ func handle_player_movement(delta) -> void:
 		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
+
+func _complete_interaction() -> void:
+	# Interaction logic here
+	print("Interaction performed!")
+	EventBus.interaction_complete.emit()
+	# Add raycast or other interaction detection logic here
