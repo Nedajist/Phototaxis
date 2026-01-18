9bc8b9388b486204133d38e407dd5be4114bdf4c
Rocky Hankin <rocdoc@gmail.com>
2026-01-18 14:03:13 -0800

    breaker switches off when floodlight dies

--- breaker_box.gd
+++ breaker_box.gd
@@ -2,6 +2,10 @@ extends StaticBody3D
 @onready var switch: Switch = %Switch
 @export var attached_electronics: Array[Node3D] 
 
+func _ready():
+	EventBus.light_burned_out.connect(_burnout_flip)
+	
+
 func interact():
 	if switch.active:
 		switch.animate_switch_off()
@@ -15,3 +19,9 @@ func interact():
 		for electronic in attached_electronics:
 			if electronic.has_method("activate"):
 				electronic.activate()
+
+func _burnout_flip(target):
+	if target in attached_electronics:
+		if switch.active:
+			switch.animate_switch_off()
+			await switch.animation_player.animation_finished
